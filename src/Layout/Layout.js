f9fdbabb601c44bcb5045410ad59455d3cf8415a
devinazhifa <devinazhifa@gmail.com>
2022-04-22 16:20:48 +0700

    first commit

---
+++ src/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+const Layout = () => {
+  return (
+      <div className='min-h-screen flex flex-col'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+  )
+}
+
+export default Layout
+
+/* Outlet : Mengeluarkan konten sesuai routenya */
